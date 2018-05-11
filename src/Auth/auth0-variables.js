f1b498fec58e89d9279cbec0447fcd669ebf3a3c
Jacob Kattampilly <jacobkuriala@gmail.com>
2018-05-11 13:55:04 -0700

    Refactored Auth

--- src/Auth/auth0-variables.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-  domain: 'drivemotors.auth0.com',
-  clientId: 'pfLIwsWrtUoFg63I5o7k3JPS7hOAB1cf',
-  callbackUrl: 'http://localhost:3013',
-  apiUrl: 'https://dev.drive:3000'
-}
